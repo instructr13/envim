917a9a6512dba23b312ee9c2fe137f6af6ca8abd
Mido <mido@wycey.dev>
2025-11-27 17:34:57 +0900

    feat(editor): add undo navigator

--- lua/plugins/normal/ui/init.lua
+++ lua/plugins/normal/ui/init.lua
@@ -354,6 +354,47 @@ return {
     },
   },
   {
+    "y3owk1n/time-machine.nvim",
+
+    cmd = {
+      "TimeMachineToggle",
+      "TimeMachinePurgeBuffer",
+      "TimeMachinePurgeAll",
+      "TimeMachineLogShow",
+      "TimeMachineLogClear",
+    },
+
+    keys = {
+      {
+        "<leader>t",
+        "",
+        desc = "Undo tree",
+      },
+      {
+        "<leader>tt",
+        "<cmd>TimeMachineToggle<cr>",
+        desc = "Toggle tree",
+      },
+      {
+        "<leader>tx",
+        "<cmd>TimeMachinePurgeCurrent<cr>",
+        desc = "Purge current",
+      },
+      {
+        "<leader>tX",
+        "<cmd>TimeMachinePurgeAll<cr>",
+        desc = "Purge all",
+      },
+      {
+        "<leader>tl",
+        "<cmd>TimeMachineLogShow<cr>",
+        desc = "Show log",
+      },
+    },
+
+    opts = {},
+  },
+  {
     "lewis6991/satellite.nvim",
 
     opts = {
